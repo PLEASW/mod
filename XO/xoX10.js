0092313081c26a3e3282983b2668f428daf22873
Duc Minh <ducminhdo08@gmail.com>
2021-02-03 11:47:38 +0700

    df

--- XO/xoX10.js
+++ XO/xoX10.js
@@ -4,3 +4,12 @@ this.options = {
   friendly_colors: 2,
   map_name: "XO",
 };
+function Pieces(side, pos) {
+  this.display = {};
+  this.pos = pos;
+  this.side = side;
+  this.isClick = Object.assign(this.display, {
+    clickable: false,
+    components: [{ type: "text", position: [0, 0, 100, 100], value: side }],
+  });
+}
