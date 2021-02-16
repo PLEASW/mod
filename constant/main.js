10778de9980f686bb6fa6b5cbc44a2acee748aa8
Duc Minh <ducminhdo08@gmail.com>
2021-02-16 07:56:14 +0700

    asdf

---
+++ constant/main.js
@@ -0,0 +1,2 @@
+const square = 0.5625;
+const tick = 1 / 60;
--- flappy bird/main.js
+++ flappy bird/main.js
@@ -4,9 +4,11 @@ this.options = {
 };
 const square = 0.5625;
 const g = 10;
-function Bird(x, y, width) {
-  this.x = x;
-  this.y = y;
+const tick = 1 / 60;
+function Bird(width) {
+  this.x = 35;
+  this.y = 100 / 2 + width;
+  this.vy = 0;
   this.shape = [];
   this.display = {
     id: "bird",
@@ -15,5 +17,59 @@ function Bird(x, y, width) {
     components: [{ type: "box", position: [0, 0, 100, 100], fill: "#fff" }],
   };
 }
-this.tick = function (game) {};
-this.event = function (event, game) {};
+function pipes(id) {
+  this.x = 100;
+  this.y = 20 + ~~(Math.random() * 41);
+  this.display = {
+    id: id,
+    position: [this.x, 0, 5, 100],
+    visible: true,
+    components: [
+      { type: "box", position: [0, 0, 100, this.y - 20], fill: "#fff" },
+      {
+        type: "box",
+        position: [0, this.y + 20, 100, 100 - this.y + 20],
+        fill: "#fff",
+      },
+    ],
+  };
+}
+let button = {
+  id: "button",
+  position: [0, 0, 0, 0],
+  visible: true,
+  clickable: true,
+  shortcut: "W",
+};
+this.tick = function (game) {
+  for (let ship of game.ships) {
+    if (!ship.custom.init) {
+      ship.custom.init = true;
+      ship.custom.pipe = [];
+      ship.custom.bird = new Bird(5);
+      ship.setUIComponent(button);
+      ship.setUIComponent({
+        id: "starting",
+        position: [0, 80, 100, 20],
+        visible: true,
+        components: [
+          {
+            type: "text",
+            position: [0, 0, 100, 100],
+            value: "Press W to begin",
+            color: "#fff",
+          },
+        ],
+      });
+    }
+    ship.setUIComponent(ship.custom.bird.display);
+  }
+};
+this.event = function (event, game) {
+  let ship = event.ship;
+  switch (event.id) {
+    case "button":
+      ship.setUIComponent({ id: "starting", visible: false });
+      break;
+  }
+};
