143668285f17591e04fb30257b3a7c664c778dd6
Duc Minh <ducminhdo08@gmail.com>
2021-02-18 12:39:32 +0700

    13

--- mining_mod -by Wolfan - In developed/main.js
+++ mining_mod -by Wolfan - In developed/main.js
@@ -10,21 +10,7 @@ this.options = {
   map_size: 100,
   release_crystal: true,
 };
-let scoreboard = {
-  id: "scoreboard",
-  components: [
-    { type: "box", position: [0, 0, 7, 100], fill: "#242526" },
-    { type: "box", position: [1, 0, 5, 0], fill: "#000" },
-    { type: "text", position: [7, 0, 7, 7], value: 0, color: "#fff" },
-    {
-      type: "text",
-      position: [7, 94, 8, 8],
-      value: gem_upgrade[0],
-      color: "#fff",
-    },
-    { type: "text", position: [1, 0, 5, 10], value: 0, color: "#fff" },
-  ],
-};
+
 let upgrades = [
   {
     id: "9",
@@ -54,34 +40,52 @@ let donate = {
     { type: "text", position: [0, 0, 100, 100], value: "W", color: "#099" },
   ],
 };
+let scoreboard = {
+  id: "scoreboard",
+  components: [
+    { type: "box", position: [0, 0, 7, 100], fill: "#242526" },
+    { type: "box", position: [1, 0, 5, 0], fill: "#000" },
+    { type: "text", position: [7, 0, 7, 7], value: 0, color: "#fff" },
+    {
+      type: "text",
+      position: [7, 94, 8, 8],
+      value: gem_upgrade[0],
+      color: "#fff",
+    },
+    { type: "text", position: [1, 0, 5, 10], value: 0, color: "#fff" },
+  ],
+};
 this.tick = function (game) {
   for (let ship of game.ships) {
     if (!ship.custom.init) {
-      ship.custom.donate = false;
+      ship.custom.donated = false; // donate or not
       ship.custom.init = true;
-      ship.custom.credit = 0;
-      ship.custom.donate = 0;
-      ship.custom.gem_donate = ~~(ship.type / 100);
+      ship.custom.credit = 0; // credit
+      ship.custom.donate = 0; // gem donated
+      ship.custom.gem_donate = ~~(ship.type / 100); // amount of gem donate each tier
     }
-    if (ship.custom.donate) {
+    if (ship.custom.donated) {
+      let [score, crystals] = Array(2).fill(
+        ship.crystals > ship.custom.gem_donate
+          ? ship.custom.gem_donate
+          : ship.crystals
+      );
+      ship.custom.credit += crystals;
+      ship.custom.donate += crystals;
       ship.set({
         idle: true,
         vx: 0,
         vy: 0,
-        score:
-          ship.score +
-          (ship.crystals - ship.custom.gem_donate > 0
-            ? ship.custom.gem_donate
-            : ship.crystals),
-        crystals:
-          ship.crystals - ship.custom.gem_donate > 0
-            ? ship.crystals - ship.custom.gem_donate
-            : 0,
+        score: ship.score + score,
+        crystals: ship.crystals - crystals,
       });
-      ship.custom.credit += ship.custom.game;
-      ship.custom.donate = true;
     } else ship.set({ idle: false });
-
+    // scoreboard.components[1].position[3] = (ship.custom.donate / 800) * 100;
+    // scoreboard.components[4].value = ship.custom.donate;
+    // scoreboard.components[4].position[3] =
+    //   scoreboard.components[1].position[3] - 10 > 0
+    //     ? scoreboard.components[1].position[3] - 10
+    //     : 0;
     ship.setUIComponent(scoreboard);
     for (let upgrade of upgrades) ship.setUIComponent(upgrade);
     ship.setUIComponent(donate);
@@ -91,7 +95,7 @@ this.event = function (event, game) {
   let ship = event.ship;
   switch (event.id) {
     case "donate":
-      ship.custom.donate = !ship.custom.donate;
+      ship.custom.donated = !ship.custom.donated;
       break;
   }
 };
