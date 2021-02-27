4c9ae49bf561192dd870479d6f66fdc159bb5885
Duc Minh <ducminhdo08@gmail.com>
2021-02-27 11:56:56 +0700

    2324

--- X7 - finished/training.js
+++ X7 - finished/training.js
@@ -9,38 +9,3 @@ this.options = {
   release_crystal: true,
   weapons_store: true,
 };
-let aliens = [10, 11, 14, 16, 17, 18];
-
-let damage = 50,
-  laser_speed = 100,
-  rate = 1.2;
-
-this.tick = function (game) {
-  game.ships.forEach((ship, i) => {
-    if (!ship.custom.init) {
-      ship.custom.init = true;
-      ship.custom.scoreboard = {
-        id: "scoreboard",
-        components: [],
-      };
-    }
-  });
-  if (game.step % 30 === 0) {
-    if (game.aliens.length < 25) {
-      game.addAlien({
-        code: aliens[0],
-        level: 2,
-        x: Math.trunc(Math.random() * this.options.map_size),
-        y: Math.trunc(Math.random() * this.options.map_size),
-      });
-      game.custom.aliens_squawn = game.step;
-    }
-    if (game.step > game.custom.aliens_squawn + 10)
-      game.aliens.forEach((alien, i) => {});
-  }
-};
-this.event = function (event, game) {
-  let ship = event.ship;
-  switch (event.name) {
-  }
-};
