ec4dff07e3d02aac682207eff7ab1d0397ee837e
Duc Minh <ducminhdo08@gmail.com>
2021-02-27 13:48:50 +0700

    34

--- X7 - finished/training.js
+++ X7 - finished/training.js
@@ -3,9 +3,16 @@ this.options = {
   map_size: 20,
   starting_ship: 605,
   max_players: 6,
-  max_level: 6,
+  max_level: 5,
   friendly_colors: 1,
   speed_mod: 1.2,
   release_crystal: true,
   weapons_store: true,
 };
+
+this.tick = function (game) {
+  if (game.step < 180 * 60) {
+    for (let ship of game.ships) { }
+  } else { }
+};
+this.event = function (event, game) { };
