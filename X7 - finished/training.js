01d912632498efac5d122108a468ca5f58b85f2f
Duc Minh <ducminhdo08@gmail.com>
2021-02-27 18:31:51 +0700

    234

--- X7 - finished/training.js
+++ X7 - finished/training.js
@@ -3,16 +3,45 @@ this.options = {
   map_size: 20,
   starting_ship: 605,
   max_players: 6,
-  max_level: 5,
+  max_level: 6,
   friendly_colors: 1,
   speed_mod: 1.2,
   release_crystal: true,
   weapons_store: true,
 };
-
-this.tick = function (game) {
-  if (game.step < 180 * 60) {
-    for (let ship of game.ships) { }
-  } else { }
+let alien = {
+  code: 10,
+  levels: 2,
+  x: 0,
+  y: 0,
+  crystal_drop: 20,
 };
-this.event = function (event, game) { };
+let difficult = {
+  damage: 100,
+  rate: 1.5,
+  laser_speed: 100,
+  shield: 150,
+};
+this.tick = function (game) {
+  if (game.aliens.length < 30 && game.ships.length) {
+    game.addAlien(alien);
+    game.custom.alien_spawn = game.step;
+  }
+  if (game.step % 30 === 0 && game.step > game.custom.alien_spawn + 10)
+    game.aliens.forEach((alien, i) => {
+      alien.set(difficult);
+    });
+};
+
+this.event = function (event, game) {
+  let ship = event.ship;
+  switch (event.name) {
+    case "ship_spawned":
+      ship.set({ stats: 66666666, shield: 300 });
+      break;
+
+    case "ship_destroyed":
+      game.aliens.forEach(alien => { })
+      break;
+  }
+};
