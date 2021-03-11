76ab814ba9a9e9e3305725caeb266abac84ff6a3
DoDucMinh1608 <77343519+DoDucMinh1608@users.noreply.github.com>
2021-03-11 22:14:56 +0700

    Add files via upload

--- mining_mod -by Wolfan - In developed/main.js
+++ mining_mod -by Wolfan - In developed/main.js
@@ -7,51 +7,85 @@ this.options = {
   release_crystal: true,
   asteroids_strength: 0.1
 };
-let upgrades = [
-  { id: "9", position: [25, 0, 40, 10], visible: true, clickable: true, shortcut: "9", components: [{ type: "box", position: [0, 0, 100, 100] }] },
-  { id: "0", position: [25, 0, 40, 10], visible: true, clickable: true, shortcut: "0", components: [{ type: "box", position: [0, 0, 100, 100] }] }
+const upgrades = [
+  { id: "9", position: [25, 0, 20, 10], visible: true, clickable: true, shortcut: "9", components: [{ type: "box", position: [0, 0, 100, 100] }] },
+  { id: "0", position: [45, 0, 20, 10], visible: true, clickable: true, shortcut: "0", components: [{ type: "box", position: [0, 0, 100, 100] }] }
 ];
 const donate = {
   id: "donate", position: [92, 46.5, 8, 7], visible: true, clickable: true, shortcut: "W",
-  components: [{ type: "box", position: [0, 0, 100, 100], fill: "#18645b34" }, { type: "text", position: [0, 0, 100, 100], value: "W", color: "#099" }]
+  components:
+    [{ type: "box", position: [0, 0, 100, 100], fill: "#f0ad4e" },
+    { type: "text", position: [0, 0, 100, 100], value: "W", color: "#0ff" }]
 };
-let scoreboard = {
-  id: "scoreboard", components: [
-    { type: "box", position: [0, 0, 7, 100], fill: "#242526" }, // full bar
-    { type: "box", position: [1, 0, 5, 0], fill: "#000" }, // progress bar
-    { type: "text", position: [7, 0, 7, 7], value: 0, color: "#fff" }, // starter value
-    { type: "text", position: [7, 93, 100, 8], value: base_cargo, color: "#fff", align: 'left' } // max value
-  ]
+const scoreboard = function () {
+  return {
+    id: "scoreboard", components: [
+      { type: "box", position: [0, 93, 100, 7], fill: "#242526" }, // full bar
+      { type: "box", position: [0, 94, 0, 5], fill: "#ff0" }, // progress bar
+      { type: "text", position: [0, 84, 7, 7], value: 0, color: '#fff' }, // starter value
+      { type: "text", position: [50, 84, 50, 8], value: base_cargo, align: 'right', color: '#fff' } // max value
+    ]
+  };
+}
+const weapons = {
+  'rockets': [],
+  'missiles': [],
+  'torpedo': [],
+  'light mines': [],
+  'heavy mines': [],
+  'Mining pod': [],
+  'Attack pod': [],
+  'Defense pod': [],
+  'Energy refill': [],
+  'Shield refill': []
 };
 this.tick = function (game) {
-  if (game.step % 10 === 0) {
-    game.ships.forEach((ship, i) => {
+  // disable upgrade
+  if (game.step % 30 === 0) for (let ship of game.ships)
+    if (ship.type > ship.custom.tier_allow * 100 && ship.crystals == ship_cargo[Math.trunc(ship.type / 100) - 1])
+      upgrades.forEach(i => ship.setUIComponent(i));
+    else upgrades.forEach(i => ship.setUIComponent({ id: i.id, visible: false }));
+  if (game.step % 60 === 0) {
+    let ships = game.ships.sort().slice(0, 7);
+    for (let ship of game.ships) {
       if (!ship.custom.init) {
+        // mining
         ship.custom.donated = false; // donate or not
-        ship.custom.credit = 0; // credit
         ship.custom.donate = 0; // gem donated
         ship.custom.tier_allow = 3;
         ship.custom.gem_donate = 1;
+        ship.custom.base_cargo = base_cargo;
+        ship.custom.scoreboard = new scoreboard;
+        // weapons
+        ship.custom.credit = 0; // credit
         ship.custom.init = true;
         ship.setUIComponent(donate);
       }
-      if (ship.type > ship.custom.tier_allow * 100 && ship.crystals == ship_cargo[Math.trunc(ship.type / 100) - 1])
-        upgrades.forEach(i => ship.setUIComponent(i));
-      else upgrades.forEach(i => ship.setUIComponent({ id: i.id, visible: false }));
+      // update scoreboard 
+      ship.custom.scoreboard.components = ship.custom.scoreboard.components.slice(0, 4);
+      ships.forEach((ship_id, index) => {
+        ship.custom.scoreboard.components.push({ type: 'player', id: ship_id.id, position: [0, index * 6, 70, 6] });
+        ship.custom.scoreboard.components.push({ type: 'text', position: [70, index * 6, 30, 6], value: ship_id.score, align: 'right' });
+      })
+      ship.setUIComponent(ship.custom.scoreboard);
+      // donating gem
       if (ship.custom.donated) {
-        if (ship.custom.donate > base_cargo) { // check if the gem donate reach the highest value
-          if (ship.custom.tier_allow < 6)
-            (ship.custom.donate -= base_cargo, base_cargo *= 2, scoreboard.components[3].value = base_cargo);
+        if (ship.custom.donate > ship.custom.base_cargo) { // check if the gem donate reach the highest value
+          if (ship.custom.tier_allow < 6) { // check if tier allow is equal or higher than 6
+            ship.custom.donate -= ship.custom.base_cargo;
+            ship.custom.base_cargo *= 2;
+            ship.custom.scoreboard.components[3].value = ship.custom.base_cargo;
+          }
           ship.custom.tier_allow++;
         }
         ship.custom.gem_donate = Math.trunc(ship.type / 100); // amount of gem donate each tier
         let crystals = ship.crystals > ship.custom.gem_donate ? ship.custom.gem_donate : ship.crystals;
-        ship.custom.credit += crystals; ship.custom.donate += crystals;
+        ship.custom.credit += crystals;
+        ship.custom.donate += crystals;
+        ship.custom.scoreboard.components[1].position[2] = (ship.custom.donate / ship.custom.base_cargo) * 100;
         ship.set({ idle: true, vx: 0, vy: 0, score: ship.score + crystals, crystals: ship.crystals - crystals });
       } else ship.set({ idle: false });
-      scoreboard.components[1].position[3] = (ship.custom.donate / base_cargo) * 100;
-      ship.setUIComponent(scoreboard);
-    });
+    };
   }
 };
 this.event = function (event, game) {
