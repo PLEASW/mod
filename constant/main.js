fd702a1d943d041d3f3debf53e5e14f4b89c33eb
Duc Minh <ducminhdo08@gmail.com>
2021-05-22 17:44:41 +0700

    werwer

--- constant/main.js
+++ constant/main.js
@@ -1,4 +1,5 @@
-const square = 0.5625;
+const square = screen_width / screen_height; // 1920 / 1080
 const tick = 1 / 60;
 const radar_radius = (90 / this.options.radar_zoom) / 2 * this.options.map_size;
+
 game.modding.commands.command_name = function () { }
