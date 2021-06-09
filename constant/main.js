f578b9c07061ec21cc8ad7131ee731f438c7f1b2
Do Duc Minh <ducminhdo08@gmail.com>
2021-06-09 18:02:16 +0700

    2234234]

--- constant/main.js
+++ constant/main.js
@@ -1,8 +1,8 @@
 const square = screen_width / screen_height; // 1920 / 1080
 const radar_radius = (90 / this.options.radar_zoom) / 2 * this.options.map_size;
 const upgrades = [
-  { id: "9", position: [25, 0, 20, 10], visible: true, clickable: true, shortcut: "9", components: [{ type: "box", position: [0, 0, 100, 100] }] },
-  { id: "0", position: [45, 0, 20, 10], visible: true, clickable: true, shortcut: "0", components: [{ type: "box", position: [0, 0, 100, 100] }] }
+  { id: "9", position: [25, 0, 20, 10], shortcut: "9", components: [{ type: "box", position: [0, 0, 100, 100] }] },
+  { id: "0", position: [45, 0, 20, 10], shortcut: "0", components: [{ type: "box", position: [0, 0, 100, 100] }] }
 ];
 const radar_radius = (map_size * 10) / radar_zoom;
 game.modding.commands.command_name = function () { }
