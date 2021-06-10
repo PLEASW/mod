f5135a4ec30ad68cea4614a6a8020a2e4f18e32c
Do Duc Minh <ducminhdo08@gmail.com>
2021-06-10 15:10:07 +0700

    23243

--- constant/main.js
+++ constant/main.js
@@ -1,5 +1,4 @@
 const square = screen_width / screen_height; // 1920 / 1080
-const radar_radius = (90 / this.options.radar_zoom) / 2 * this.options.map_size;
 const upgrades = [
   { id: "9", position: [25, 0, 20, 10], shortcut: "9", components: [{ type: "box", position: [0, 0, 100, 100] }] },
   { id: "0", position: [45, 0, 20, 10], shortcut: "0", components: [{ type: "box", position: [0, 0, 100, 100] }] }
@@ -9,3 +8,6 @@ game.modding.commands.command_name = function () { }
 const posConvt = function (x, y) {
   return [map_size * 5 + x, map_size * 5 - y - 1];
 };
+const posConvt2 = function (x, y) {
+  return [x - map_size * 5, map_size * 5 - y - 1];
+}
