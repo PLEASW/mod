95bc890a4949bd18407a55fc801ba32e63fccf26
Do Duc Minh <ducminhdo08@gmail.com>
2021-06-11 12:41:14 +0700

    132123

--- new mod/another version.js
+++ new mod/another version.js
@@ -35,6 +35,39 @@ const upgrades = [
   { id: "9", position: [25, 0, 20, 10], visible: true, clickable: true, shortcut: "9", components: [{ type: "box", position: [0, 0, 100, 100] }] },
   { id: "0", position: [45, 0, 20, 10], visible: true, clickable: true, shortcut: "0", components: [{ type: "box", position: [0, 0, 100, 100] }] }
 ];
+function createYard(maxpoints, minheight, minwidth, fillAll) {
+  var data = { uis: [], game: [] };
+  var rand = function (num) { return Math.floor(Math.random() * num); };
+  var addBox = function (pos) { (fillAll || rand(2)) && data.uis.push(pos); };
+  var yieldsh = Math.trunc(100 / minheight), yieldsw = Math.trunc(100 / minwidth);
+  var randoms = new Array(yieldsw).fill(0).map((i, x) => new Array(yieldsh).fill(0).map((j, y) => [x * minwidth, y * minheight])).flat();
+  randoms.splice(0, 1);
+  //create a random set of points on the 100x100 grid
+  maxpoints = Math.min(yieldsw * yieldsh - 1, maxpoints);
+  while (randoms.length > maxpoints) randoms.splice(rand(randoms.length), 1);
+  randoms.sort((a, b) => a[1] - b[1]);
+
+  var startx = 0, starty = 0;
+  while (randoms.length > 0) {
+    let curpos = randoms[0], nextpos = randoms[1] || [];
+    addBox([startx, starty, curpos[0] - startx, curpos[1] - starty]);
+    startx = curpos[0];
+    if (curpos[1] != nextpos[1]) {
+      addBox([startx, starty, 100 - startx, curpos[1] - starty]);
+      startx = 0; starty = curpos[1];
+    }
+    randoms.splice(0, 1);
+  }
+  if (starty < 100) addBox([0, starty, 100, 100 - starty]);
+  const posConvt2 = function (x, y, width, height) {
+    [x, y] = [x + width * 0.5, y + height * 0.5];
+    return [x - 50, 50 - y].map((i, b) => i * map_size / 10 - b).concat([width, height].map(i => i - 0.25));
+  };
+  for (let ui of data.uis) data.game.push(posConvt2(...ui));
+
+  return data;
+}
+let yard = new createYard(50, 10, 8, 0);
 const RADAR_BACKGROUND = function () {
   this.radar = function (number) {
     let result = [];
@@ -45,7 +78,9 @@ const RADAR_BACKGROUND = function () {
   };
   this.realRadar = {
     id: 'radar_background',
-    components: this.radar(10)
+    components: this.radar(10).concat(yard.uis.map(i => {
+      return { type: 'box', position: i, fill: 'rgba(255, 255, 255, 0.2)', width: 0.5, stroke: 'rgba(0 0 0 0)' };
+    }))
   };
 };
 const RADAR_UI = function (ship) {
@@ -124,7 +159,9 @@ this.tick = function (game) {
         ship.custom.init = true;
       }
       if (ship.custom.view_radar) {
-        ship.custom.uiRadar.components = ship.custom.drawRadar(game.ships, ship).concat(game.custom.radar(10));
+        ship.custom.uiRadar.components = ship.custom.drawRadar(game.ships, ship).concat(game.custom.radar(10), yard.uis.map(i => {
+          return { type: 'box', position: i, fill: 'rgba(255, 255, 255, 0.2)', width: 0.5, stroke: 'rgba(0 0 0 0)' };
+        }));
         ship.setUIComponent(ship.custom.uiRadar);
       } else ship.setUIComponent({ id: 'radar', visible: false });
     }
