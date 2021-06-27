0912be747ec731a3b6bc0ed89f012fbacf123791
Do Duc Minh <ducminhdo08@gmail.com>
2021-06-27 17:41:55 +0700

    234

--- sb/TeamLock.js
+++ sb/TeamLock.js
@@ -1,6 +1,5 @@
 this.TeamMode.prototype.updateStation = function (t) {
   var e, i, s, l, n, a, o, r, h, u, d, c, p, I, O;
-  for (let i of this.teams) i.open = true;
   for (i = 1, u = this.teams, s = 0, a = u.length; s < a; s++)
     for (I = u[s], p = I.station_model, i += 1, p.setLevel(t.getUint8(i), this.l11lI), i += 1, p.IIlII = t.getUint32(i, !0), i += 4, p.crystals_max = this.options.crystal_capacity[p.level], i += 1, d = p.modules, l = 0, o = d.length; l < o; l++) h = d[l], O = t.getUint8(i), i += 1, h.setAlive(O > 0, this.l11lI), h.setShield(Math.max(0, O - 1) / 254, this.l11lI);
   if (null != this.team && this.teams.length > 1) {
