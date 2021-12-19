4bbf4028056033013e521273363b72f13a6fe41f
Do Duc Minh <you@example.com>
2021-12-19 15:01:35 +0700

    234

--- X7 - finished/testing/features/shiptree.js
+++ X7 - finished/testing/features/shiptree.js
@@ -2,13 +2,13 @@ function shipCode(ships, name) {
   const keys = Object.keys(ships ?? {});
   return {
     id: name,
-    ships() {
+    ships(order = 1) {
       return this.codes = Array(keys.length).fill(0)
-        .map((i, r) => JSON.parse(ships[keys[r]]).typespec.code).sort((a, b) => a - b);
+        .map((i, r) => JSON.parse(ships[keys[r]]).typespec.code).sort((a, b) => (a - b) * order);
     },
-    event(ship = { type: 0 }) {
-      this.codes || this.ships();
-      const type = this.codes[this.codes.indexOf(ship.type) + 1] ?? this.codes[0];
+    event(ship) {
+      this.codes ?? this.ships(-1);
+      const type = this.codes[this.codes.indexOf(ship?.type) + 1] ?? this.codes[0];
       ship.set({ type, stats: type < 700 ? 88888888 : 0 });
     }
   }
