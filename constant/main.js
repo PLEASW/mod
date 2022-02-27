a0cde7a4bdc8f853d32e6c545e326bc947d7b2e8
Your Name <you@example.com>
2022-02-27 18:18:43 +0700

    234

--- constant/main.js
+++ constant/main.js
@@ -3,8 +3,8 @@ const upgrades = [
   { id: "9", position: [25, 0, 20, 10], shortcut: "9", components: [{ type: "box", position: [0, 0, 100, 100] }] },
   { id: "0", position: [45, 0, 20, 10], shortcut: "0", components: [{ type: "box", position: [0, 0, 100, 100] }] }
 ];
-// const radarRadius = (mapSize * 10) / radarZoom;
-// game.modding.commands.command_name = function () { }
+const radarRadius = (mapSize * 10) / radarZoom;
+game.modding.commands.command_name = function () { }
 let mapSize = 200;
 let width = 1;
 const mapToComponent = function (x, y) {
@@ -15,13 +15,4 @@ const componentToMap = function (x, y) {
   return [x - 50, 50 - y].map((i, b) => i * mapSize / 10 - b);
 };
 ; (function (params) { })(); // custom command for use in mod development
-// $.get("path/to/file.js").then(data => eval(data)).catch(e => echo("Failed wtf"))
-const sendUI = function (ship, UI) {
-  if (ship != null && typeof ship.setUIComponent == "function") {
-    if (UI.visible || UI.visible == null) ship.setUIComponent(UI);
-    else ship.setUIComponent({ id: UI.id, position: [0, 0, 0, 0], visible: false });
-  }
-};
-const [x, y] = mapToComponent(0, 0);
-const [x0, y0] = componentToMap(x, y);
-console.log({ x, y }, { x0, y0 });
+$.get("path/to/file.js").then(data => eval(data)).catch(e => echo("Failed wtf"))
